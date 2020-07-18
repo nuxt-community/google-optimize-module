9eb578d027a4ddf853d6fd71cb160b35bbbc0cde
Josh Deltener <josh.deltener@realtruck.com>
2020-07-18 18:05:24 -0500

    made empty user agent test actually empty

--- test/bot-block.test.js
+++ test/bot-block.test.js
@@ -49,7 +49,7 @@ describe('basic', () => {
   })
 
   test('empty user agent', async () => {
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
+    await page.setUserAgent('')
     await page.goto(url('/'))
 
     const $exp = await page.evaluate(() => window.$exp)
